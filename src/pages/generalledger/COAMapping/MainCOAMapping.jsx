76f21646a06bacc234a5c2fa5d094780e3c9b614
bryandchristofer <bryandchsinaga@gmail.com>
2024-12-13 11:28:40 +0700

    add Main COA Mapping page

---
+++ src/pages/generalledger/COAMapping/MainCOAMapping.jsx
@@ -0,0 +1,39 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+const MainCOAMapping = () => {
+  return (
+    <div className="container bg-white p-8 mx-auto my-4 rounded-lg w-15/16">
+      <div className="flex justify-center gap-4">
+        <NavLink
+          to="coa-mapping"
+          className={({ isActive }) =>
+            `font-bold flex-1 px-3 py-3 text-center border rounded transition-all duration-300 ${
+              isActive
+                ? "bg-custom-blue text-white shadow-lg scale-105"
+                : "bg-white text-custom-blue hover:bg-gray-300 hover:text-black"
+            }`
+          }
+        >
+          COA Mapping
+        </NavLink>
+        <NavLink
+          to="coa-division"
+          className={({ isActive }) =>
+            `font-bold flex-1 px-3 py-3 text-center border rounded transition-all duration-300 ${
+              isActive
+                ? "bg-custom-blue text-white shadow-lg scale-105"
+                : "bg-white text-custom-blue hover:bg-gray-300 hover:text-black"
+            }`
+          }
+        >
+          COA Division
+        </NavLink>
+      </div>
+      <div className="flex-1">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default MainCOAMapping;
