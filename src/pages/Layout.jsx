4f0ac7a08bb0c38b53968f1d3c3c3e16454063be
Juanda Ritonga <ritongajuan102@gmail.com>
2024-12-20 16:13:37 +0700

    Update File Name

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,5 +1,5 @@
-import Sidebar from "../components/sidebarxxx";
-import Header from "../components/headerxxx";
+import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
 import "../index.css";
